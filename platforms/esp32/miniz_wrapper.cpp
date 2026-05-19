52b97d291b61c42356aa5d6f9acc07bb62b39066
CidVonHighwind <Patricks.96@hotmail.de>
2026-05-19 18:06:04 +0200

    fixed esp32 build

--- platforms/esp32/miniz_wrapper.cpp
+++ platforms/esp32/miniz_wrapper.cpp
@@ -1,8 +1,11 @@
 // Compile miniz (C code) as C++ so the IDF toolchain's C++-only flags
-// (e.g. -fuse-cxa-atexit) do not cause compiler errors.
+// (e.g. -fuse-cxa-atexit) do not cause errors when compiling .c files.
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wtype-limits"
 extern "C" {
-#include "../../test/build2/_deps/miniz-src/miniz.c"
+#include "../../third_party/miniz/miniz.c"
+#include "../../third_party/miniz/miniz_tdef.c"
+#include "../../third_party/miniz/miniz_tinfl.c"
+#include "../../third_party/miniz/miniz_zip.c"
 }
 #pragma GCC diagnostic pop
